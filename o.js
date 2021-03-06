e202f649d6cf3dee571c804078d717ff81b531ca
gsndk <80062726+gsndk@users.noreply.github.com>
2021-03-06 21:08:45 +0100

    Update o.js

--- o.js
+++ o.js
@@ -1,1 +1,1 @@
-{"preb":true,"prez":true,"preq":true,"prei":"","preo":"","pref":false,"preu":1,"prex":"","prek":"","pem":"","ree":"com.tencent.ig","s1":"","a1":false,"a2":false,"a3":false,"b1":false,"b2":false,"b3":false,"b4":false,"b5":false}
+{"preb":true,"prez":true,"preq":true,"prei":"","preo":"","pref":false,"preu":1,"prex":"","prek":"","pem":"","ree":"com.dts.freefireth","s1":"","a1":false,"a2":false,"a3":false,"b1":true,"b2":false,"b3":false,"b4":false,"b5":false}
